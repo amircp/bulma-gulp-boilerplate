16bf845b585e3dbdb1dc6c5a428ffb60946ffac3
Amir <amir@healthlabs.com>
2021-05-22 19:17:28 -0500

    Boiler template gulp-sass-bulma css

---
+++ gulpfile.js
@@ -0,0 +1,46 @@
+
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const cleanCSS = require('gulp-clean-css');
+const concat = require('gulp-concat');
+const bless = require('gulp-bless');
+const clean = require('gulp-clean');
+
+const $ = require('gulp-load-plugins')();
+
+
+
+gulp.task('sass', () => {
+    return gulp
+      .src(`./src/assets/styles/styles.scss`)
+      .pipe(
+        $.sass({
+          includePaths: [
+            './node_modules/bulma/bulma'
+          ],
+          outputStyle: 'compressed'
+        }).on('error', $.sass.logError)
+      )
+    .pipe(gulp.dest(`./dist/assets/styles/`));
+  });
+
+  gulp.task('html-templates', () => {
+    return gulp
+      .src(`./src/*.html`)
+      .pipe(gulp.dest(`./dist/`));
+  });
+
+
+  gulp.task('clean', function() {
+    return gulp.src(['./dist'], { read: false, allowEmpty: true }).pipe(clean());
+  });
+
+  gulp.task(
+    'setup',
+    gulp.series(
+      'clean',
+      gulp.parallel('sass'),
+      'html-templates'
+    )
+  );
+  
