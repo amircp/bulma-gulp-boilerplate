97eea0d147a4146e3883ce631b988c5b0c3baed7
Amir <hello@amircanto.com>
2021-09-23 21:08:22 -0500

    Add support for image assets and js compressing

--- gulpfile.js
+++ gulpfile.js
@@ -1,44 +1,62 @@
-
+/**
+ * 
+ * @author Amir Oswaldo Canto Palomo
+ * @description Gulp Task Boiler Plate to Compile Bulma sass files and compress js files
+ * 
+ */
 const gulp = require('gulp');
 const sass = require('gulp-sass');
 const bless = require('gulp-bless');
 const clean = require('gulp-clean');
-
 const $ = require('gulp-load-plugins')();
-
-
+const uglify = require('gulp-uglify');
 
 gulp.task('sass', () => {
     return gulp
-      .src(`./src/assets/styles/styles.scss`)
-      .pipe(
-        $.sass({
-          includePaths: [
-            './node_modules/bulma/bulma'
-          ],
-          outputStyle: 'compressed'
-        }).on('error', $.sass.logError)
-      )
-    .pipe(gulp.dest(`./dist/assets/styles/`));
-  });
+        .src(`./src/assets/styles/*.scss`)
+        .pipe(
+            $.sass({
+                includePaths: [
+                    './node_modules/bulma/bulma'
+                ],
+                outputStyle: 'compressed'
+            }).on('error', $.sass.logError)
+        )
+        .pipe(gulp.dest(`./dist/assets/styles/`));
+});
 
-  gulp.task('html-templates', () => {
+gulp.task('html-templates', () => {
     return gulp
-      .src(`./src/*.html`)
-      .pipe(gulp.dest(`./dist/`));
-  });
+        .src(`./src/*.html`)
+        .pipe(gulp.dest(`./dist/`));
+});
 
+gulp.task('image-assets', () => {
+    return gulp
+        .src(`./src/assets/images/*`)
+        .pipe(gulp.dest(`./dist/assets/images/*`));
+});
 
-  gulp.task('clean', function() {
-    return gulp.src(['./dist'], { read: false, allowEmpty: true }).pipe(clean());
-  });
+gulp.task('js-assets', () => {
+    return gulp.src('./src/assets/js/*')
+        .pipe(uglify())
+        .pipe(gulp.dest('./dist/assets/js/'));
+});
 
-  gulp.task(
+gulp.task('clean', function() {
+    return gulp.src(['./dist'], {
+        read: false,
+        allowEmpty: true
+    }).pipe(clean());
+});
+
+gulp.task(
     'setup',
     gulp.series(
-      'clean',
-      gulp.parallel('sass'),
-      'html-templates'
+        'clean',
+        gulp.parallel('sass'),
+        'html-templates',
+        'image-assets',
+        'js-assets'
     )
-  );
-  
+);
